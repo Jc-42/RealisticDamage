07d0c07afef9aa1f34058c39a6864e4aa376dd54
Jc-42 <jc.julian.carpenter@gmail.com>
2024-07-09 17:49:24 -0500

    Cleaned up extranious code and added more comments

--- src/main/java/com/github/Jc42/realisticdamage/RealisticDamage.java
+++ src/main/java/com/github/Jc42/realisticdamage/RealisticDamage.java
@@ -51,11 +51,10 @@ public class RealisticDamage
     public static final double VANILLA_BASE_SPEED = 0.10000000149011612;
     public static boolean keyPacketHandled = true;
     public static boolean releasedMovementKeyMidair = false;
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "realisticdamage";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    //Constructor
     public RealisticDamage()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -63,38 +62,22 @@ public class RealisticDamage
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-//        // Register the Deferred Register to the mod event bus so blocks get registered
-//        BLOCKS.register(modEventBus);
-//        // Register the Deferred Register to the mod event bus so items get registered
-//        ITEMS.register(modEventBus);
-//        // Register the Deferred Register to the mod event bus so tabs get registered
-//        CREATIVE_MODE_TABS.register(modEventBus);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-//        // Register the item to a creative tab
-//        modEventBus.addListener(this::addCreative);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    //Register packet handler
     private void commonSetup(final FMLCommonSetupEvent event)
     {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         event.enqueueWork(PacketHandler::register);
-
-
-//        if (Config.logDirtBlock)
-//            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-//
-//        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-//
-//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
+    //Update pain level and send pain packet
     @SubscribeEvent
     public static void onLivingHurt(LivingHurtEvent event) {
         if (event.getEntity() instanceof Player) {
@@ -115,18 +98,13 @@ public class RealisticDamage
     }
 
 
-    // Run every server tick
+    //Lower pain level with packets and player speed. Also stop player from sprinting and moving midair
     @SubscribeEvent
     public static void onServerTick(TickEvent.ServerTickEvent event) {
         MinecraftServer server = event.getServer();
         if (event.phase == TickEvent.Phase.START) {
             for (ServerPlayer player : server.getPlayerList().getPlayers()) {
                 player.getCapability(PainCapabilityProvider.PAIN_CAPABILITY).ifPresent(pain -> {
-//                    if(pain.getAdrenalineLevel() > 0) {
-//                        pain.addAdrenaline(-.15f); //Adrenaline lowers by 3 per second
-//                        if(pain.getAdrenalineLevel() < 0) pain.setAdrenalineLevel(0);
-//                        if(pain.getAdrenalineLevel() > 100) pain.setAdrenalineLevel(100);
-//                    }
                     if(pain.getChronicPainLevel() > 0) {
 //                        pain.addChronicPain(-.05f); //Chronic pain lowers by 1 per second
                         if(pain.getChronicPainLevel() < 0) pain.setChronicPainLevel(0);
@@ -165,19 +143,14 @@ public class RealisticDamage
     }
 
 
+    //Remove the sprint attribute, may be redundant
     @SubscribeEvent
     public static void onLivingTick(LivingEvent.LivingTickEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            AttributeInstance movementSpeed = player.getAttribute(Attributes.MOVEMENT_SPEED);
-            if (movementSpeed != null) {
-                Set<AttributeModifier> s = movementSpeed.getModifiers();
-                for (AttributeModifier modifier : movementSpeed.getModifiers()) {
-                    //player.sendSystemMessage(Component.literal("UUID: " + modifier.getId() + " Name: " + modifier.getName()));
-                }
-            }
 
-//            UUID SPRINT_SPEED_BOOST_ID = UUID.fromString("662A6B8D-DA3E-4C1C-8813-96EA6097278D");
+            AttributeInstance movementSpeed = player.getAttribute(Attributes.MOVEMENT_SPEED);
             UUID SPRINT_SPEED_BOOST_ID = UUID.fromString("662a6b8d-da3e-4c1c-8813-96ea6097278d");
+
             if (movementSpeed != null) {
                 // Check if the player has high chronic pain
                 player.getCapability(PainCapabilityProvider.PAIN_CAPABILITY).ifPresent(pain -> {
@@ -190,15 +163,11 @@ public class RealisticDamage
         }
     }
 
+    //Client code
     @Mod.EventBusSubscriber(modid = RealisticDamage.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
     public static class ClientModEventBusEvents {
 
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Client-side initialization
-        }
-
-        // Stop player from sprinting after 20 pain
+        //Stop player from sprinting after 20 pain
         @SubscribeEvent
         @OnlyIn(Dist.CLIENT)
         public static void onClientTickEvent(TickEvent.ClientTickEvent event) {
@@ -213,6 +182,9 @@ public class RealisticDamage
             }
         }
 
+        //Un-press the sprint key when the player shouldn't sprint.
+        //This IS required in addition to the server side code
+        //as Minecraft REALLY doesn't want you to stop the sprint event
         @SubscribeEvent
         @OnlyIn(Dist.CLIENT)
         public static void onClientTick(TickEvent.ClientTickEvent event) {
@@ -234,21 +206,19 @@ public class RealisticDamage
             }
         }
 
+        //Determine if the player let go of movement midair
+        //Needed to tell if we should resume the movement when the player lands
+        //Otherwise the player will keep moving even if they let go
         @SubscribeEvent
         @OnlyIn(Dist.CLIENT)
         public static void onKeyInput(InputEvent.Key event) {
             Minecraft minecraft = Minecraft.getInstance();
             if (minecraft.player != null) {
-                //minecraft.player.sendSystemMessage(Component.literal("w: " + minecraft.options.keyUp.isDown()));
-            }
-            if (minecraft.player != null) {
                 if (event.getKey() == minecraft.options.keyUp.getKey().getValue()) {
                     if (!minecraft.player.onGround() && event.getAction() == GLFW.GLFW_PRESS) {
-                        //minecraft.player.sendSystemMessage(Component.literal( "Released false"));
                         releasedMovementKeyMidair = false;
                     }
                     if (!minecraft.player.onGround() && event.getAction() == GLFW.GLFW_RELEASE) {
-                        //minecraft.player.sendSystemMessage(Component.literal( "Released true"));
                         releasedMovementKeyMidair = true;
                     }
                 }
@@ -256,117 +226,23 @@ public class RealisticDamage
         }
     }
 
-//    @SubscribeEvent
-//    public void onPlayerMovementInput(PlayerMovementInputEvent event) {
-//        Player player = event.getPlayer();
-//        Input input = event.getInput();
-//
-//        // Prevent movement input if the player is in midair and is going faster than base speed
-//        // Used to stop the player from jumping to move faster when in pain
-//        player.getCapability(PainCapabilityProvider.PAIN_CAPABILITY).ifPresent(pain -> {
-//            if(!player.onGround()) {
-//                double horizontalSpeed = Math.sqrt(Math.pow(player.getDeltaMovement().x, 2) + Math.pow(player.getDeltaMovement().z, 2));
-//                // Used to scale the speed loss from midair such that at <=20 pain there is no effect and >=50 there is no speed bonus
-//                double speedLossScale = Math.min((Math.max(pain.getChronicPainLevel() - 20, 0)) / 30, 1);
-//                LOGGER.debug("!!! speed scale : " + speedLossScale);
-//                if (horizontalSpeed * speedLossScale > Objects.requireNonNull(player.getAttribute(Attributes.MOVEMENT_SPEED)).getBaseValue()) {
-//                    event.setCanceled(true);
-//                    LOGGER.debug("!!! canceled");
-//
-//                    // Stop movement under a certain value
-//                    // This is a hacky way to prevent the little amount of movement that still occurs
-//                    // even after canceling which I cant figure out the source of
-//
-//                    if (Math.abs(player.getDeltaMovement().x) < .03) {
-//                        Vec3 velocity = player.getDeltaMovement();
-//                        velocity = new Vec3(-player.getDeltaMovement().x * .8, velocity.y, velocity.z);
-//                        player.setDeltaMovement(velocity);
-//                    }
-//                    if (Math.abs(player.getDeltaMovement().z) < .03) {
-//                        Vec3 velocity = player.getDeltaMovement();
-//                        velocity = new Vec3(velocity.x, velocity.y, -player.getDeltaMovement().z * .8);
-//                        player.setDeltaMovement(velocity);
-//                    }
-//                } else {
-//                    event.setCanceled(false);
-//                }
-//            }
-//
-//        });
-//    }
-
+    //Attach the pain capability
     @SubscribeEvent
     public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> attachEvent) {
         if (attachEvent.getObject() instanceof Player) {
             if (!attachEvent.getObject().getCapability(PainCapabilityProvider.PAIN_CAPABILITY).isPresent()) {
-                LOGGER.debug("Pain Attached");
                 attachEvent.addCapability(new ResourceLocation(MODID, "pain"), new PainCapabilityProvider());
             }
         }
     }
 
-
+    //Register the pain capability
     @Mod.EventBusSubscriber(modid = RealisticDamage.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ModEventBusEvents {
         @SubscribeEvent
         public static void registerCapabilities(RegisterCapabilitiesEvent event) {
             event.register(IPainCapability.class);
             LOGGER.debug("Registering pain capabilities");
-        }
-    }
-
-
-
-
-
-//    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-//    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-//    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
-//    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-//
-//    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-//    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-//    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-//    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-//
-//    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-//    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-//            .alwaysEat().nutrition(1).saturationMod(2f).build())));
-//
-//    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
-//    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-//            .withTabsBefore(CreativeModeTabs.COMBAT)
-//            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-//            .displayItems((parameters, output) -> {
-//                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-//            }).build());
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-//        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-//            event.accept(EXAMPLE_BLOCK_ITEM);
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
         }
     }
 }
